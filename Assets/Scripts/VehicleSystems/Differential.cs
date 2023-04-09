e31e374a5ba492334ab53d07cf607da8eda0c15e
LAPTOP-SVEN\Sven <svenboeckx2@gmail.com>
2023-04-09 22:36:50 +0200

    Create Differential.cs

---
+++ Assets/Scripts/VehicleSystems/Differential.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace OVP.VehicleSystems
+{
+    public class Differential : MonoBehaviour
+    {
+        [SerializeField] private float m_DifferentialGearRatio = 3.9f; // Gear ratio of the differential
+
+        // Get output torque for the left output shaft based on the input torque
+        public float GetOutputTorqueLeft(float inputTorque)
+        {
+            // Symmetrical open differential
+            return inputTorque * m_DifferentialGearRatio * 0.5f;
+        }
+
+        // Get output torque for the right output shaft based on the input torque
+        public float GetOutputTorqueRight(float inputTorque)
+        {
+            // Symmetrical open differential
+            return inputTorque * m_DifferentialGearRatio * 0.5f;
+        }
+
+        // Calculate the input shaft velocity based on the output shaft velocities of left and right sides
+        public float GetInputShaftVelocity(float outputShaftVelocityLeft, float outputShaftVelocityRight)
+        {
+            // Input shaft velocity is the sum of left and right output shaft velocities
+            // multiplied by half the differential gear ratio to account for distribution of torque
+            return outputShaftVelocityLeft + outputShaftVelocityRight * 0.5f * m_DifferentialGearRatio;
+        }
+    }
+}
